0d5ceee7e543304ff77d0454eae8f28deb6a3b53
maddyonline <madhavjha@gmail.com>
2015-09-19 02:17:51 -0700

    more

--- prepare/prepare.go
+++ prepare/prepare.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 	_ "time"
 )
 
@@ -21,20 +22,54 @@ func download() {
 	client := github.NewClient(tc)
 	owner := "maddyonline"
 	repo := "fun-with-algo"
-	filepath := "testit/v1/Can_string_be_palindrome_gen.cc"
+	filepath := "testit/v1"
 	opt := &github.RepositoryContentGetOptions{"b04c4c9ba6d9548df666913e6b2f6a164ad03cfe"}
-	r, err := client.Repositories.DownloadContents(owner, repo, filepath, opt)
+
+	url, _, err := client.Repositories.GetArchiveLink(owner, repo, github.Zipball, opt)
 	if err != nil {
 		log.Fatal(err)
 	}
-	d1, err := ioutil.ReadAll(r)
+
+	downloadArchive := func() {
+		log.Printf("Downloading: %s\n", url)
+		cmd := exec.Command("curl", "-o", "abc.zip", "-O", url.String())
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.Run()
+	}
+	extractArchive := func() {
+		log.Printf("Extracting")
+		cmd := exec.Command("unzip", "abc.zip", "-d", "unique_dir/")
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.Run()
+	}
+
+	downloadArchive()
+	os.Mkdir("unique_dir", 0777)
+	extractArchive()
+
+	name, err := exec.Command("ls", "unique_dir").CombinedOutput()
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = ioutil.WriteFile("dl_data", d1, 0644)
-	if err != nil {
-		log.Fatal(err)
+	log.Printf("Found the name: %s\n", name)
+
+	dl := func() {
+		r, err := client.Repositories.DownloadContents(owner, repo, filepath, opt)
+		if err != nil {
+			log.Fatal(err)
+		}
+		d1, err := ioutil.ReadAll(r)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = ioutil.WriteFile("dl_data", d1, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
+	dl()
 }
 
 func main() {
